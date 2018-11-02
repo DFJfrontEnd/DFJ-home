7bcdc77557cb9f2956084e2db6c08a31b0945e85
leehaizhong <15007173189@163.com>
2018-11-02 14:54:42 +0800

    up 中英文切换

---
+++ src/assets/i18n/zh.js
@@ -0,0 +1,18 @@
+/**
+ *官网中文语言信息提取
+ *
+ * 1.json格式，涉及项目内中文提取信息均在此修改
+ * 2.需严格遵循如下级别格式：页面>组件>功能组>功能点，如：home:{ navigation: { title:'鼎丰居官网' //鼎丰居官网 } }
+ * 3.非中文翻译需按照各信息点后面(//之后)的备注文字进行翻译
+ * 4.html页面中使用方法：在模板引擎中统一使用 {{$t('home.navigation.title')}}
+ * 5.在js中使用：统一使用 this.$t('home.navigation.title') 替换
+ */
+const zhMessages = {
+  home: {
+    title: '中文的品牌名' //中文的品牌名
+  },
+}
+
+export {
+  zhMessages
+}
--- src/main.js
+++ src/main.js
@@ -3,13 +3,31 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import VueI18n from 'vue-i18n'
+import {zhMessages} from "./assets/i18n/zh"
+import {enMessages} from "./assets/i18n/en"
 
-Vue.config.productionTip = false
+Vue.use(VueI18n);
+
+Vue.config.productionTip = false;
+
+//i18n中英文切换管理
+window.i18n = new VueI18n({
+  locale: 'en',
+  messages: {
+    zh: zhMessages,
+    en: enMessages
+  }
+})
 
 /* eslint-disable no-new */
-new Vue({
+window.appVue = new Vue({
   el: '#app',
+  data: {
+    eventHub: new Vue() //
+  },
   router,
-  components: { App },
-  template: '<App/>'
+  i18n,
+  components: {App},
+  template: '<App/>',
 })
